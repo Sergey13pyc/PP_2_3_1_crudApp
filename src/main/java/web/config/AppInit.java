186746b46e0c2b3362981835b69dff8f23e3f0a1
Sergey Artemov <sergey560113@gmail.com>
2023-08-21 17:20:26 +0300

    Добавил: -отображение всех -отображение одного -добавление пользователя

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,6 +1,11 @@
 package web.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -25,5 +30,25 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+    // Ниже два метода добавлены д/того, чтобы на стороне Спринга обрабатывалось скрытое hidden-поле "_method",
+    // где находится реальный http-метод, который мы хотим использовать. В нашем случае - PATCH.
+    //Это сделано д/того, чтобы корректно работал метод update в контроллере, у которого аннотация @PatchMapping ("/{id}")
+    //Д/его корректной работы мы ниже создаем фильтр, который будет читать скрытое hidden-поле "_method",
+    // значение которого будет PATCH
+    //В Спринг Boot эти методы можно будет заменить одной строкой
 
+    @Override
+    public void onStartup(ServletContext context) throws ServletException {
+        super.onStartup(context);
+        registerHiddenFilter(context);
+    }
+
+    public void registerHiddenFilter(ServletContext context) {
+        context.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
+                .addMappingForUrlPatterns(null, true, "/*");
+
+        context.addFilter("characterEncodingFilter",
+                        new CharacterEncodingFilter("UTF-8", true, true))
+                .addMappingForUrlPatterns(null, false, "/*");
+    }
 }
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,8 +3,8 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
 import web.service.UserService;
 
 @Controller
@@ -22,5 +22,23 @@ public class UserController {
             model.addAttribute("users", userService.getAllUsers());
             System.out.println("Returning 'all' view"); // Временная строка для логирования
             return "all";
+    }
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("users", userService.getUserById(id));
+        return "one";
+    }
+    @GetMapping("/add")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "add";
+
+    }
+
+    @PostMapping()
+    public String createUser(@ModelAttribute("user") User user){
+        userService.addUser(user);
+        return "redirect:/users";
+
     }
 }
